bad213aa15c048a14ecb3c3810177026434277eb
Shorif Ahmed <sashipon001@gmail.com>
2022-01-01 11:00:14 +0600

    update

--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
+
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms'
+
 
 @Component({
   selector: 'app-new-component',
@@ -24,6 +25,8 @@ color1 = 'green';
 getData(data:NgForm){
   console.log(data);
 }
+
+
 
 getname11:any= '';
 getName11(data:any){
@@ -42,43 +45,6 @@ addMeFirst(data:any){
  console.log(data); 
 }
 
-
-
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
-=======
-import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms'
-
-@Component({
-  selector: 'app-new-component',
-  templateUrl: './new-component.component.html',
-  styleUrls: ['./new-component.component.css']
-})
-export class NewComponentComponent implements OnInit {
-
-title = "Multiple condition or else if";
-// color="red";
-color="green";
-
-
-//switch case
-
-color1 = 'green';
-
-
-//Form
-
-getData(data:NgForm){
-  console.log(data);
-}
-
-
 // todo list
 
 list:any=[];
@@ -94,23 +60,13 @@ removeTask(id:number){
   this.list=this.list.filter((item: { id: number; })=>item.id!==id);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
->>>>>>> 254e55672b2700e80a27504d7def434396361db8
+
+
+
+
